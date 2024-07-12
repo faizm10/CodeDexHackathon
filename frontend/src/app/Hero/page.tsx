0e45cfaeac09ce16ce8dbf49fb15416f33c49234
faizm10 <faizmustansar10@gmail.com>
2024-07-12 11:19:49 -0400

    cleanup

--- frontend/src/app/Hero/page.tsx
+++ frontend/src/app/Hero/page.tsx
@@ -8,17 +8,18 @@ const Hero: React.FC = () => {
         className="text-yellow-500 font-sans font-bold text-6xl mb-20 mt-20 p-5"
         style={{ textShadow: "2px 2px 4px yellow" }}
       >
-        <em>Welcome to the 2024 Paris Olympics Predictor!</em>
+        <em>Welcome to OlympiPredict!</em>
       </h1>
+      <h2 className="text-white font-sans font-medium text-4xl mb-10">
+        Hackathon Project for CodeDex
+      </h2>
       <a href="/predictor">
-
         <button
           id="heroButton"
           className="bg-purple-600 text-yellow-500 px-20 py-5 rounded-xl font-sans text-xl font-semibold hover:bg-yellow-500 hover:text-purple-800 transition-colors hover:shadow-lg"
         >
           Enter Predictor Now!
         </button>
-
       </a>
       <img src="/images/hero/olympicbg.png" alt="Olympic Background" />
     </div>
