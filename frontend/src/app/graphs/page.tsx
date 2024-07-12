72a9e3de33dbaf2b9814805f7e3de0d86b6327eb
faizm10 <faizmustansar10@gmail.com>
2024-07-12 11:39:23 -0400

    FINAL

--- frontend/src/app/graphs/page.tsx
+++ frontend/src/app/graphs/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { FaGithub, FaHeart } from "react-icons/fa";
 
 const Graphs: React.FC = () => {
@@ -9,10 +10,13 @@ const Graphs: React.FC = () => {
       </h1>
       <div className="flex flex-col items-center w-full max-w-6xl">
         <div className="bg-white rounded-lg shadow-lg overflow-hidden m-4 w-full">
-          <img
+          <Image
             src="/all_medals.png"
             alt="Graph 1"
-            className="w-full h-full object-cover"
+            layout="responsive"
+            width={700}
+            height={400}
+            className="object-cover"
           />
           <div className="p-4 text-center">
             <h2 className="text-2xl font-bold mb-2">
@@ -25,17 +29,20 @@ const Graphs: React.FC = () => {
           </div>
         </div>
         <div className="bg-white rounded-lg shadow-lg overflow-hidden m-4 w-full">
-          <img
+          <Image
             src="/allCanadaMedals.png"
             alt="Graph 2"
-            className="w-full h-full object-cover"
+            layout="responsive"
+            width={700}
+            height={400}
+            className="object-cover"
           />
           <div className="p-4 text-center">
             <h2 className="text-2xl font-bold mb-2">2024 Predicted Medals</h2>
             <p className="text-gray-700">
-              "This graph displays the distribution of Canadian Olympic medals
+              This graph displays the distribution of Canadian Olympic medals
               (Gold, Silver, and Bronze) from 1900 to 2020, highlighting the
-              performance trends over time."
+              performance trends over time.
             </p>
           </div>
         </div>
